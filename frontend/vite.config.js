db2480160c7afdd1825755d952f04ed28b556e2d
Leo Palmeira <leandro2703palmeira@gmail.com>
2026-05-15 11:58:26 -0300

    Set base path for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/campainha-digital-full/',
   plugins: [
     react(),
     VitePWA({
